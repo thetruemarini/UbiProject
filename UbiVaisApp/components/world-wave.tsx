0a74caa461d2638212124195ca24d5be7bcea53d
riki <r.marini006@studenti.unibs.it>
2025-10-11 06:15:26 +0200

    impl worldwave

--- UbiVaisApp/components/world-wave.tsx
+++ UbiVaisApp/components/world-wave.tsx
@@ -1,18 +1,31 @@
-import Animated from 'react-native-reanimated';
+import { useEffect } from 'react';
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 export function WorldWave() {
+  const rotation = useSharedValue(0);
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withTiming(360, { duration: 5000 }),
+      -1 // -1 = infinito
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotation.value}deg` }],
+  }));
+
   return (
     <Animated.Text
-      style={{
-      fontSize: 28,
-      lineHeight: 32,
-      marginTop: -6,
-      animationName: {
-        '100%': { transform: [{ rotate: '360deg' }] },
-      },
-      animationIterationCount: 1,
-      animationDuration: '2000ms',
-      }}>
+      style={[
+        {
+          fontSize: 28,
+          lineHeight: 32,
+          marginTop: -6,
+        },
+        animatedStyle,
+      ]}
+    >
       🌍
     </Animated.Text>
   );
